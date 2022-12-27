b500e34092397d4ca05601545e4ab7621ab8fe78
Taras Kizlo <taras@kiflo.com>
2022-12-27 16:49:16 +0200

    M

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more informationfn
+﻿// See https://aka.ms/new-console-template for more informationfnf
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World2!");
 Console.WriteLine("Master");
