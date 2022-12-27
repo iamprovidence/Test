8547775ac886562e150e2bbe9c66fd6d25251a77
Taras Kizlo <taras@kiflo.com>
2022-12-27 16:23:42 +0200

    M

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more informations
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World2!");
